f0dd6472874d2c972d14d3fae5bd420978b44ee3
wjwow007 <yiwoojungDev@gmail.com>
2022-08-19 22:25:39 +0900

    Add tsconfig.json

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import BoardPage from "./pages/board";
+
+function App() {
+  return <BoardPage />;
+}
+
+export default App;
---
+++ src/pages/board/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function BoardPage() {
+  return (
+    <div>
+      <div>board</div>
+    </div>
+  );
+}
